5887312b073e35571a0d65a02477a9913a225d6b
Audren <audren.destin@gmail.com>
2024-03-28 21:17:27 +0100

    quête 4 on lâche rien

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,5 @@
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "Mew",
-    },
-];
 
-function PokemonCard() {
-    const pokemon = pokemonList[1]; /* création d'une variable qui contient le premier Pokemon de la liste*/
-
+function PokemonCard({ pokemon }) {
     return (
         <div>
             {pokemon.imgSrc ? (<figure>
@@ -19,19 +7,10 @@ function PokemonCard() {
                 <figcaption>{pokemon.name}</figcaption>
             </figure>) : (<figure> <figcaption>{pokemon.name}</figcaption><p>???</p></figure>)}
 
-        </div>                          /* les données de la variable pokemon remplacent les attributs fixes - opérateur ternaire */
+        </div>
     );
 }
 
 export default PokemonCard;
 
 
-
-/* function MyTitle() {
-
-  return <h1>Hello React</h1>;
-
-}
-
-
-export default MyTitle; */
